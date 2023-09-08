a0239692c3d2c82f77bfc5bda14c8389bc711544
qimijoy <qimijoy@yandex.ru>
2023-09-08 22:06:33 +0300

    stations api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,62 @@
 <template>
 	<main class="main">
-		<p>ID: {{ postId }}</p>
-		<p>userID: {{ userId }}</p>
-		<p>title: {{ postTitle }}</p>
-		<p>ID: {{ postBody }}</p>
+		<ul class="main__lines">
+			<li v-for="line of metroData" :key="line.id" class="line">
+				<span class="line__info">
+					<span class="line__info-name">{{ line.name }} ветка</span>
+					<span class="line__info-color" :style="{ backgroundColor: `#${line.hex_color}` }" />
+				</span>
+
+				<ul class="line__stations">
+					<li v-for="station of line.stations" :key="station.id" class="main__line-station">
+						<p>{{ station.name }}</p>
+					</li>
+				</ul>
+			</li>
+		</ul>
 	</main>
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from 'vue';
+import { ref, onMounted } from 'vue';
 
-const postData = ref({});
+const metroData = ref({});
 
 onMounted(async () => {
-	fetch('https://jsonplaceholder.typicode.com/posts/1')
+	fetch('https://api.hh.ru/metro/1')
 		.then((response) => response.json())
-		.then((json) => (postData.value = json))
-		.catch((error) => (postData.value = error));
+		.then(({ lines }) => (metroData.value = lines))
+		.catch((error) => (metroData.value = error));
 });
-
-const postId = computed(() => postData.value.id);
-const userId = computed(() => postData.value.userId);
-const postTitle = computed(() => postData.value.title);
-const postBody = computed(() => postData.value.body);
 </script>
 
-<style class="scoped">
+<style class="scoped" lang="less">
 .main {
 	display: flex;
 	height: 100%;
 	flex-direction: column;
 	justify-content: center;
-	align-items: flex-start;
+	align-items: center;
+}
+
+.line {
+	&__info {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	&__info-name {
+		margin-right: 5px;
+	}
+
+	&__info-color {
+		width: 15px;
+		height: 15px;
+	}
+
+	&__stations {
+		padding-inline-start: 20px;
+	}
 }
 </style>
