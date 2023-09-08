d1b4d48e36816f8e713b8d32dbea01ac14812121
qimijoy <qimijoy@yandex.ru>
2023-09-08 09:36:20 +0300

    delete files

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
-<script setup>
-import { RouterView } from 'vue-router';
-import GoHeader from './components/GoHeader/GoHeader.vue';
-</script>
-
 <template>
 	<GoHeader></GoHeader>
 	<RouterView />
 </template>
 
-<style scoped></style>
+<script setup>
+import { RouterView } from 'vue-router';
+import GoHeader from './components/GoHeader/GoHeader.vue';
+</script>
