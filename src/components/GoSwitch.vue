4cb05388404eb28f013c0de47cba261060fb6f4d
qimijoy <qimijoy@yandex.ru>
2023-09-18 22:28:28 +0300

    switch

--- src/components/GoSwitch.vue
+++ src/components/GoSwitch.vue
@@ -1,51 +1,25 @@
 <template>
-	<div class="switch-theme"></div>
+	<div class="switch" :class="{ switch_on: isOn }" @click="isOn = !isOn" />
 </template>
 
 <script setup>
-(function () {
-	const switchTheme = document.querySelector('.switch-theme');
-	const body = document.querySelector('body');
+import { ref } from 'vue';
 
-	changeLocalStorageTheme();
-
-	switchTheme.addEventListener('click', toggleTheme);
-
-	function changeLocalStorageTheme() {
-		// Initial theme setting
-		if (!localStorage.getItem('theme')) {
-			localStorage.setItem('theme', 'light');
-		}
-
-		// Check, if dark theme
-		if (localStorage.getItem('theme') === 'dark') {
-			toggleTheme();
-		}
-	}
-
-	function toggleTheme() {
-		switchTheme.classList.toggle('dark-theme');
-		body.classList.toggle('dark-theme');
-
-		if (body.classList.contains('dark-theme')) {
-			localStorage.setItem('theme', 'dark');
-		} else {
-			localStorage.setItem('theme', 'light');
-		}
-	}
-})();
+const isOn = ref(false);
 </script>
 
-<style scoped lang="scss">
-.switch-theme {
+<style scoped lang="less">
+@import '@/styles/colors';
+
+.switch {
 	display: inline-block;
 	position: relative;
 	width: 50px;
 	height: 25px;
-	border: 2px solid $switch-theme-border-light-color;
+	border: 2px solid @black;
 	background: transparent;
 	border-radius: 25px;
-	box-shadow: 1px 1px 2px 0px (rgba($switch-theme-border-light-color, 0.5));
+	box-shadow: 1px 1px 2px 0 (rgba(@black, 0.5));
 	cursor: pointer;
 
 	&::before {
@@ -53,48 +27,23 @@
 		position: absolute;
 		top: 1px;
 		left: 2px;
-		width: 23px;
-		height: 23px;
-		background: $switch-theme-border-light-color;
+		width: 19px;
+		height: 19px;
+		background: @black;
 		border-radius: 50%;
 		transition: 0.5s;
 	}
 
-	&.dark-theme {
-		border-color: $switch-theme-border-dark-color;
-		background: $switch-theme-border-light-color;
-		box-shadow: 1px 1px 2px 0px (rgba($switch-theme-border-dark-color, 0.5));
+	&_on {
+		border-color: @white;
+		background: @black;
+		box-shadow: 1px 1px 2px 0 (rgba(@white, 0.5));
 		transition: 0.5s;
 
 		&::before {
 			left: 25px;
-			background: $switch-theme-border-dark-color;
+			background: @white;
 		}
 	}
 }
-
-// Main colors
-$main-color-dark: #222222;
-$main-color-light: #f4f4f4;
-
-// Light theme colors
-$background-color-light-theme: $main-color-light;
-$background-color-dark-theme: $main-color-dark;
-
-// Dark theme colors
-$font-color-light-theme: $main-color-dark;
-$font-color-dark-theme: $main-color-light;
-
-// Switch theme colors
-$switch-theme-border-light-color: $main-color-dark;
-$switch-theme-border-dark-color: $main-color-light;
-
-// Progress bar colors
-$progress-bar-height: 5px;
-$progress-bar-main-color: orange;
-$progress-bar-current-color: orangered;
-
-// Custom modal colors
-$custom-modal-mask-bg-color: #cccccce6;
-$custom-modal-bg-color: #eee;
 </style>
