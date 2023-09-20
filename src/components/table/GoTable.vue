af7ce6984d707627b1b7149167086b33e6af35c5
qimijoy <qimijoy@yandex.ru>
2023-09-20 09:40:10 +0300

    table init

---
+++ src/components/table/GoTable.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="table-container">
+		<div class="table">
+			<div class="table__header"></div>
+		</div>
+	</div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="less"></style>
