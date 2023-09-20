bbc0fca71e8e682bd4a1510308457d7398d05987
qimijoy <qimijoy@yandex.ru>
2023-09-20 09:35:11 +0300

    tabs component

--- src/components/controls/GoTabs.vue
+++ src/components/controls/GoTabs.vue
@@ -1,57 +1,79 @@
 <template>
-	<div>
-		<div class="tab-nav">
+	<div class="tabs">
+		<div class="tabs__nav">
 			<span
-				v-for="tab in names"
+				v-for="tab in tabs"
 				:key="tab.name"
-				:class="['tab-nav__item', { selected: tab.name === selectedTab }]"
+				:class="['tabs__nav-item', { 'tabs__nav-item_selected': tab.name === selectedTab }]"
 				@click="clickOnTab(tab.name)"
-				>{{ tab.label }}
+			>
+				{{ tab.label }}
 			</span>
 		</div>
-		<div class="tab-content">
+		<div class="tabs__content">
 			<slot />
 		</div>
 	</div>
 </template>
+
 <script setup>
+// Props
 const props = defineProps({
-	names: {
+	tabs: {
 		type: Array,
 		require: true,
 	},
+	selectedTab: {
+		type: String,
+	},
 });
+
+// Emits
+const emits = defineEmits({
+	changeTab: (tabName) => !!tabName,
+});
+
+// Functions
+const clickOnTab = (tabName) => {
+	emits('changeTab', tabName);
+};
 </script>
 
 <style scoped lang="less">
-.tab {
-	&-nav {
+@import '@/styles/colors';
+
+.tabs {
+	&__nav {
 		display: flex;
 		align-items: center;
 		margin-bottom: 20px;
-		&__item {
-			height: 40px;
-			display: flex;
-			align-items: center;
-			justify-content: center;
-			margin-right: 10px;
-			border-radius: 7px;
-			cursor: pointer;
-			border: 1px solid var(--primary);
-			padding: 0 20px;
-			font-size: 15px;
-			&:hover {
-				background: var(--primary-hover);
-				color: #fff;
-				transition: 0.2s;
-			}
-			&.selected {
-				background: var(--primary);
-				color: #fff;
-			}
+	}
+
+	&__nav-item {
+		height: 40px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin-right: 10px;
+		border-radius: 7px;
+		cursor: pointer;
+		border: 1px solid @primary;
+		padding: 0 20px;
+		font-size: 15px;
+
+		&:hover {
+			background: @primary-hover;
+			color: #fff;
+			transition: 0.2s;
 		}
 	}
-	&-content {
+
+	&__nav-item_selected {
+		background: @primary;
+		color: #fff;
+	}
+
+	&__content {
 		padding: 20px;
 		border-radius: 7px;
 		background: #fff;
