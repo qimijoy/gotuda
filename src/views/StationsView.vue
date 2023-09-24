0c381524a6b18f7b545e1c96591a9588f89cdf5f
qimijoy <qimijoy@yandex.ru>
2023-09-24 17:49:06 +0300

    stations page

--- src/views/StationsView.vue
+++ src/views/StationsView.vue
@@ -2,24 +2,21 @@
 	<main>
 		<GoHeaderH1>{{ $t('sections.stations.header-text') }}</GoHeaderH1>
 
-		<!--		<div class="stations">-->
-		<!--			<ul class="stations__list">-->
-		<!--				<li v-for="line of lines" :key="line.id" class="stations__line line">-->
-		<!--					<span class="line__info">-->
-		<!--						<span class="line__info-color" :style="{ backgroundColor: `#${line.hex_color}` }" />-->
-		<!--						<span class="line__info-name">{{ line.name }} ветка</span>-->
-		<!--					</span>-->
-
-		<!--					<ul class="line__stations">-->
-		<!--						<li v-for="station of line.stations" :key="station.id" class="main__line-station">-->
-		<!--							<p>{{ station.name }}</p>-->
-		<!--						</li>-->
-		<!--					</ul>-->
-		<!--				</li>-->
-		<!--			</ul>-->
-		<!--		</div>-->
-
-		<GoAccordion :data="data" />
+		<div class="lines">
+			<GoSpoiler v-for="line of lines" :key="line.id" :data="line" class="line">
+				<template #summary>
+					<span class="line__info">
+						<span class="line__info-color" :style="{ backgroundColor: `#${line.hex_color}` }" />
+						<span class="line__info-name">{{ line.name }} [{{ line.stations.length }}]</span>
+					</span>
+				</template>
+				<template #content>
+					<ul class="stations">
+						<li v-for="station of line.stations" :key="station.id" class="station">{{ station.name }}</li>
+					</ul>
+				</template>
+			</GoSpoiler>
+		</div>
 	</main>
 </template>
 
@@ -29,41 +26,30 @@ import { useI18n } from 'vue-i18n';
 import { useMainStore } from '@/stores/MainStore';
 
 import GoHeaderH1 from '@/components/GoHeaderH1.vue';
-import GoAccordion from '@/components/GoAccordion.vue';
+import GoSpoiler from '@/components/GoSpoiler.vue';
 
 // STATES
 const mainStore = useMainStore();
 const { t } = useI18n();
-
-const data = [
-	{
-		id: 1,
-		summary: 'What is ',
-		content: 'The capital sdfs f',
-		isOpen: false,
-	},
-	{
-		id: 2,
-		summary: 'What is the tallest',
-		content: 'The tallest animal ',
-		isOpen: true,
-	},
-	{
-		id: 3,
-		summary: 'What is the tallest',
-		content: 'The tallest animal ',
-		isOpen: false,
-	},
-];
 
 // COMPUTED
 const lines = computed(() => mainStore.lines);
 </script>
 
 <style lang="less" scoped>
-.stations {
-	&__list {
-		list-style: none;
+@import '@/assets/styles/_breakpoints';
+
+.lines {
+	display: grid;
+	grid-template-columns: 1fr;
+	gap: 15px 20px;
+
+	@media @small {
+		grid-template-columns: 1fr 1fr;
+	}
+
+	@media @large {
+		grid-template-columns: 1fr 1fr 1fr;
 	}
 }
 
@@ -77,11 +63,16 @@ const lines = computed(() => mainStore.lines);
 	&__info-color {
 		width: 15px;
 		height: 15px;
-		margin-right: 5px;
+		margin-right: 10px;
+		border-radius: 6px;
 	}
+}
 
-	&__stations {
-		padding-inline-start: 20px;
-	}
+.stations {
+	list-style: none;
+}
+
+.station {
+	margin-bottom: 4px;
 }
 </style>
