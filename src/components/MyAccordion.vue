caf5229055b71fbc0b69d209da5765e86b9d35bd
qimijoy <qimijoy@yandex.ru>
2023-10-26 17:37:53 +0300

    save accordion from extinction

---
+++ src/components/MyAccordion.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="accordion">
+		<div class="accordion__title" @click="isOpen = !isOpen">{{ title }}</div>
+		<div v-if="isOpen" class="accordion__text">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const props = defineProps({
+	title: {
+		type: String,
+		required: true,
+	},
+});
+
+const isOpen = ref(false);
+</script>
+
+<style scoped lang="less">
+.accordion {
+	display: inline-block;
+	text-align: center;
+
+	&__title {
+		font-weight: bold;
+		font-size: 20px;
+		margin-bottom: 20px;
+		cursor: pointer;
+	}
+}
+</style>
